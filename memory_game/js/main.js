4718c3d3bb5081aac47df1a08e432734a539eb66
Johnathon Sykes <hireme@mrjsykes.com>
2017-05-11 14:15:49 -0700

    created additional game logic # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is ahead of 'origin/master' by 1 commit. # Changes to be committed: # # 	modified:   memory_game/js/main.js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,26 +6,49 @@
 
 
 
-var cards = ["queen", "queen","king","king"];
+var cards = [{
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: "images/king-of-diamond.png"
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: "images/queen-of-hearts.png"
+  }
+];
 var cardsInPlay = [];
 
-function checkForMatch(){
+function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("We have a match!")
+    alert("We have a match!")
   } else {
-      alert("Sorry, try again");
+    alert("Sorry, try again");
   }
 }
 
 
-function flipCard(cardId){
-  console.log("User flipped " +cards[cardId]);
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardId].rank);
 
-  cardsInPlay.push(cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
 
   if (cardsInPlay.length === 2) {
     checkForMatch()
   }
+
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 }
 
 flipCard(0);
